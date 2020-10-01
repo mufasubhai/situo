ea832a10eee9a0cf895e86569b9e0e18b5b4b56a
Adrian Apodaca <aapodaca@gmail.com>
2020-09-30 19:11:20 -0700

    remove pw from form

--- frontend/components/user/profile_settings_form_container.jsx
+++ frontend/components/user/profile_settings_form_container.jsx
@@ -14,7 +14,7 @@ class ProfileSettingsForm extends React.Component {
     }
 
     componentDidMount() {
-        
+        this.setState({password2: "", password3: ""})
     }
 
     handleChange(field) {
@@ -44,14 +44,25 @@ class ProfileSettingsForm extends React.Component {
         e.preventDefault();
         const formData = new FormData();
         if (this.state.photoFile) {
-            formData.append('user[photo]', this.state.photoFile)
-            formData.append('user[password]', this.state.password)
-            formData.append('user[email]', this.state.email)
-            formData.append('user[name]', this.state.name)
-            this.props.updateUserPhoto(this.props.user.id, formData);
+
+            if (this.state.password2 !== this.state.password3) {
+                alert('Passwords must match')
+            } else {
+                formData.append('user[photo]', this.state.photoFile)
+                formData.append('user[password]', this.state.password)
+                formData.append('user[email]', this.state.email)
+                formData.append('user[name]', this.state.name)
+                this.props.updateUserPhoto(this.props.user.id, formData);
+
+            }
 
         } else {
-            this.props.updateUser(this.state)
+            if (this.state.password2 !== this.state.password3) {
+                alert('Passwords must match')
+            } else {
+
+                this.props.updateUser(this.state)
+            }
         }
         
 
@@ -61,43 +72,91 @@ class ProfileSettingsForm extends React.Component {
     render () {
         console.log(this.state)
         return (
-            <div className="modal_container">
+          <div className="modal_container">
+            <span className="modal_top">
+              <h1 className="edit_title">Profile Settings</h1>
 
-                <span className="modal_top">
-                    <h1 className="edit_title">Profile Settings</h1>
+              <div
+                className="modal_close_trigger"
+                onClick={() => this.props.closeModal()}
+              >
+                <svg
+                  className="x_arrow_svg"
+                  focusable="false"
+                  viewBox="0 0 32 32"
+                >
+                  <path
+                    className="x_arrow_svg"
+                    d="M18.1,16L27,7.1c0.6-0.6,0.6-1.5,0-2.1s-1.5-0.6-2.1,0L16,13.9l-8.9-9C6.5,4.3,5.6,4.3,5,4.9S4.4,6.4,5,7l8.9,8.9L5,24.8c-0.6,0.6-0.6,1.5,0,2.1c0.3,0.3,0.7,0.4,1.1,0.4s0.8-0.1,1.1-0.4l8.9-8.9l8.9,8.9c0.3,0.3,0.7,0.4,1.1,0.4s0.8-0.1,1.1-0.4c0.6-0.6,0.6-1.5,0-2.1L18.1,16z"
+                  ></path>
+                </svg>
+              </div>
+            </span>
 
-                    <div className="modal_close_trigger" onClick={() => this.props.closeModal()}>
-                        <svg className="x_arrow_svg" focusable="false" viewBox="0 0 32 32"><path className="x_arrow_svg" d="M18.1,16L27,7.1c0.6-0.6,0.6-1.5,0-2.1s-1.5-0.6-2.1,0L16,13.9l-8.9-9C6.5,4.3,5.6,4.3,5,4.9S4.4,6.4,5,7l8.9,8.9L5,24.8c-0.6,0.6-0.6,1.5,0,2.1c0.3,0.3,0.7,0.4,1.1,0.4s0.8-0.1,1.1-0.4l8.9-8.9l8.9,8.9c0.3,0.3,0.7,0.4,1.1,0.4s0.8-0.1,1.1-0.4c0.6-0.6,0.6-1.5,0-2.1L18.1,16z"></path></svg>
-                    </div>
+            <span className="project_edit_bottom">
+              <label className="modal_label">
+                Name
+                <span className="spacer"></span>
+                <input
+                  type="text"
+                  className="project_name_update text_input"
+                  value={this.state.name}
+                  onChange={this.handleChange("name")}
+                />
+              </label>
+              <label className="modal_label">
+                email
+                <span className="spacer"></span>
+                <input
+                  type="text"
+                  className="project_name_update text_input"
+                  value={this.state.email}
+                  onChange={this.handleChange("email")}
+                />
+              </label>
+              {/* <label className="modal_label">
+                Password
+                <span className="spacer"></span>
+                <input
+                  type="password"
+                  className="project_name_update text_input"
+                  value={this.state.password}
+                  onChange={
+                    (this.handleChange("password"),
+                    this.handleChange("password2"))
+                  }
+                />
+              </label>
+              <label className="modal_label">
+                Confirm Password
+                <span className="spacer"></span>
+                <input
+                  type="password"
+                  className="project_name_update text_input"
+                  value={this.state.password}
+                  onChange={
+                    (this.handleChange("password3"))
+                  }
+                />
+              </label> */}
+              <label className="modal_label">
+                Upload photo
+                <span className="spacer"></span>
+                <input
+                  className="update-project-modal-button edit_project"
+                  type="file"
+                  onChange={this.handleFile}
+                />
+              </label>
 
-                </span>
-
-                <span className="project_edit_bottom">
-
-                    <label className="modal_label" >Name
-                    <span className="spacer"></span>
-                        <input type="text" className="project_name_update text_input" value={this.state.name} onChange={this.handleChange('name')} />
-                    </label>
-                    <label className="modal_label" >email
-                    <span className="spacer"></span>
-                        <input type="text" className="project_name_update text_input" value={this.state.email} onChange={this.handleChange('email')} />
-                    </label>
-                    <label className="modal_label" >Password
-                    <span className="spacer"></span>
-                        <input type="password" className="project_name_update text_input" value={this.state.password} onChange={this.handleChange('password')} />       
-                    </label>
-                    <label className="modal_label" >Upload photo
-                    <span className="spacer"></span>
-                        <input className="update-project-modal-button edit_project" type="file" onChange={this.handleFile}/>
-                    </label>
-
-                    <div className="update-project-modal-button">
-                        <button className="edit_project" onClick={this.handleSubmit} >Update Profile
-                    </button>
-                    </div>
-                </span>
-            </div>
-        )
+              <div className="update-project-modal-button">
+                <button className="edit_project" onClick={this.handleSubmit}>
+                  Update Profile
+                </button>
+              </div>
+            </span>
+          </div>
+        );
     }
 }
 
